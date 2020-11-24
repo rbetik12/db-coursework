65d14efab8efc72b7c0a16c1568c05a6a5db0207
Vitaliy <mr.prikota@gmail.com>
2020-11-24 21:56:32 +0300

    Fixed that generator sometimes generate inventory inserts that violate unique constraint

--- generators/inventory_generator.py
+++ generators/inventory_generator.py
@@ -10,11 +10,17 @@ class InventoryGenerator(AbstractGenerator):
         super().__init__()
         self.actorIds = actorIds
         self.itemIds = itemIds
+        self.pickedItemActor = []
 
     def generate(self, amount: int) -> None:
         for i in range(amount):
+            actorId = self.get_random_actor_id()
+            itemId = self.get_random_item_id()
+            if (actorId, itemId) in self.pickedItemActor:
+                continue
+            self.pickedItemActor.append((actorId, itemId))
             self.queries += f"insert into inventory(actor_id, item_id, amount)" \
-                            f" values ({self.get_random_actor_id()}, {self.get_random_item_id()}, " \
+                            f" values ({actorId}, {itemId}, " \
                             f"{random.randint(1, 100)});\n"
 
     def get_query(self) -> str:
