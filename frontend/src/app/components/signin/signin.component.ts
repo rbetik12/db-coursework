5a34950d9bdc814bf73ddbe1a3cf3ac7f74e0bc4
Vitaliy <mr.prikota@gmail.com>
2020-12-19 16:50:23 +0300

    Added FormControl for sign in form

--- frontend/src/app/components/signin/signin.component.ts
+++ frontend/src/app/components/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Player} from '../../models/player.model';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-signin',
@@ -8,11 +10,47 @@ import {Component, OnInit} from '@angular/core';
 export class SigninComponent implements OnInit {
 
     public hide = true;
+    public signInForm = new FormGroup(
+        {
+            usernameOrEmail: new FormControl(
+                '',
+                [Validators.required]
+            ),
+            password: new FormControl(
+                '',
+                [Validators.required]
+            )
+        }
+    );
 
     constructor() {
     }
 
     ngOnInit(): void {
+    }
+
+    onSubmit(event: Event): void {
+        event.preventDefault();
+        const player: Player = {
+            id: null,
+            email: '',
+            password: '',
+            username: ''
+        };
+        if (this.signInForm.controls.usernameOrEmail.hasError('required')
+            || this.signInForm.controls.password.hasError('required')) {
+            return;
+        }
+        const usernameOrEmail: string = this.signInForm.controls.usernameOrEmail.value;
+        const password: string = this.signInForm.controls.password.value;
+        if (usernameOrEmail.includes('@')) {
+            player.email = usernameOrEmail;
+        }
+        else {
+            player.username = usernameOrEmail;
+        }
+        player.password = password;
+        console.log(player);
     }
 
 }
---
+++ frontend/src/app/models/player.model.ts
@@ -0,0 +1,6 @@
+export interface Player {
+    id: number | null;
+    email: string;
+    password: string;
+    username: string;
+}
