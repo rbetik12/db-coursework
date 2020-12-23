bae8526cdf12b6a39b2fbaa378fa731e28eee041
Vitaliy <mr.prikota@gmail.com>
2020-12-24 00:08:39 +0300

    Added clan info panel

--- frontend/src/app/components/clan-page/clan-page.component.ts
+++ frontend/src/app/components/clan-page/clan-page.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Globals} from '../../injectables/globals.config';
+import {Clan} from '../../models/clan.model';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
     selector: 'app-clan-page',
@@ -9,12 +12,38 @@ import {ActivatedRoute} from '@angular/router';
 export class ClanPageComponent implements OnInit {
 
     public clandId: string | null = '';
+    public clan: Clan = {id: 0, name: '', rating: 0, type: '', region: {name: '', id: 0}};
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute,
+                private router: Router,
+                private globals: Globals,
+                private http: HttpClient) {
     }
 
     ngOnInit(): void {
         this.clandId = this.route.snapshot.paramMap.get('id');
+
+        this.fetchClanInfo();
+    }
+
+    public fetchClanInfo(): void {
+        this.http.get(this.globals.address + this.globals.port + '/api/clan/info',
+            {
+                withCredentials: true,
+                params: {id: this.clandId as string}
+            }).subscribe(
+                res => {
+                    this.clan = res as Clan;
+                }
+        );
+    }
+
+    public toProfile(): void {
+        this.router.navigateByUrl('/profile');
+    }
+
+    public toClansList(): void {
+        this.router.navigateByUrl('/clans');
     }
 
 }
--- frontend/src/app/models/clan.model.ts
+++ frontend/src/app/models/clan.model.ts
@@ -1,6 +1,9 @@
+import {Region} from './region.model';
+
 export interface Clan {
     id: number | null;
     name: string;
     type: string;
     rating: number;
+    region: Region;
 }
