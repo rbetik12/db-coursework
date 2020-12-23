a5db53a83be54b1e58b56e5f2c5e0365c0e54909
Vitaliy <mr.prikota@gmail.com>
2020-12-23 22:38:55 +0300

    Added clan controller

---
+++ backend/src/main/java/io/rbetik12/eengine/controller/ClanController.java
@@ -0,0 +1,30 @@
+package io.rbetik12.eengine.controller;
+
+import io.rbetik12.eengine.entity.Clan;
+import io.rbetik12.eengine.service.ClanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clan")
+@CrossOrigin
+public class ClanController {
+
+    @Autowired
+    private final ClanService clanService;
+
+    public ClanController(ClanService clanService) {
+        this.clanService = clanService;
+    }
+
+    @GetMapping(path = "all", produces = "application/json")
+    public ResponseEntity<List<Clan>> getClansList() {
+        return ResponseEntity.ok(clanService.getAll());
+    }
+}
